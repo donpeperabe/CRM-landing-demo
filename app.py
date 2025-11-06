2e257f990a6f89a87211f5ff5d25bfac4d801e31
donpeperabe <donpeperabe@gmail.com>
2025-11-06 11:49:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -880,4 +880,5 @@ create_default_crm_user()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
